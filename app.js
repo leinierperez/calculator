3c245ee625245652649dd85458386d3f9ac37bee
Leinier Perez <P_Leinier@yahoo.com>
2021-09-03 02:52:51 -0400

    Linting

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ function multiply(a, b) {
 
 function divide(a, b) {
   if (b === 0 || isNaN(b)) {
-    throw new Error("Divide by zero Error");
+    throw new Error('Divide by zero Error');
   }
   return a / b;
 }
@@ -20,42 +20,42 @@ function divide(a, b) {
 //takes an operator and 2 numbers and then calls one of the above functions on the numbers.
 function operate(operator, a, b) {
   switch (operator) {
-    case "add":
+    case 'add':
       return add(a, b);
-    case "subtract":
+    case 'subtract':
       return subtract(a, b);
-    case "multiply":
+    case 'multiply':
       return multiply(a, b);
-    case "divide":
+    case 'divide':
       return divide(a, b);
     default:
-      console.log("Choose an Operator");
+      console.log('Choose an Operator');
   }
 }
 
-const display = document.querySelector(".display");
-const btns = document.querySelectorAll(".calculator button");
+const display = document.querySelector('.display');
+const btns = document.querySelectorAll('.calculator button');
 
 btns.forEach((button) => {
-  button.addEventListener("click", (e) =>
+  button.addEventListener('click', (e) =>
     calculatorDisplay(button.textContent)
   );
 });
 
 const keyCodeToOp = {
-  49: "1",
-  50: "2",
-  51: "3",
-  52: "4",
-  53: "5",
-  54: "6",
-  55: "7",
-  56: "8",
-  57: "9",
-  190: ".",
-  8: "backspace",
+  49: '1',
+  50: '2',
+  51: '3',
+  52: '4',
+  53: '5',
+  54: '6',
+  55: '7',
+  56: '8',
+  57: '9',
+  190: '.',
+  8: 'backspace',
 };
-window.addEventListener("keydown", (e) => {
+window.addEventListener('keydown', (e) => {
   calculatorDisplay(keyCodeToOp[e.keyCode]);
 });
 
@@ -63,8 +63,8 @@ function removeZeros(str) {
   return Number(str).toString();
 }
 
-let firstNum = "";
-let secondNum = "";
+let firstNum = '';
+let secondNum = '';
 let operator;
 let output;
 let decimalCount = true;
@@ -72,21 +72,21 @@ let decimalCount = true;
 function calculatorDisplay(value) {
   function addDigit(value) {
     if (operator) {
-      if (secondNum === "" && value === ".") {
-        secondNum = "0";
+      if (secondNum === '' && value === '.') {
+        secondNum = '0';
       }
       secondNum += value;
-      if (value === ".") {
+      if (value === '.') {
         display.innerHTML = secondNum;
       } else {
         display.innerHTML = removeZeros(secondNum);
       }
     } else {
-      if (firstNum === "" && value === ".") {
-        firstNum = "0";
+      if (firstNum === '' && value === '.') {
+        firstNum = '0';
       }
       firstNum += value;
-      if (value === ".") {
+      if (value === '.') {
         display.innerHTML = firstNum;
       } else {
         display.innerHTML = removeZeros(firstNum);
@@ -100,35 +100,35 @@ function calculatorDisplay(value) {
         output = operate(operator, Number(firstNum), Number(secondNum));
         display.innerHTML = output;
         firstNum = output.toString();
-        secondNum = "";
+        secondNum = '';
       }
       decimalCount = true;
     } catch (err) {
-      display.innerHTML = "Error";
-      firstNum = "";
-      secondNum = "";
+      display.innerHTML = 'Error';
+      firstNum = '';
+      secondNum = '';
       operator = undefined;
     }
   }
 
   if (value.match(/^[0-9.]$/)) {
-    if (value === "." && decimalCount) {
+    if (value === '.' && decimalCount) {
       decimalCount = false;
       addDigit(value);
-    } else if (value !== ".") {
+    } else if (value !== '.') {
       addDigit(value);
     }
   } else {
     switch (value) {
-      case "C":
-        display.innerHTML = "0";
-        firstNum = "";
-        secondNum = "";
+      case 'C':
+        display.innerHTML = '0';
+        firstNum = '';
+        secondNum = '';
         operator = undefined;
         decimalCount = true;
         break;
-      case "backspace":
-        if (display.innerHTML.endsWith(".")) {
+      case 'backspace':
+        if (display.innerHTML.endsWith('.')) {
           decimalCount = true;
         }
         display.innerHTML = display.innerHTML.slice(0, -1);
@@ -138,23 +138,23 @@ function calculatorDisplay(value) {
           firstNum = firstNum.slice(0, -1);
         }
         break;
-      case "-":
+      case '-':
         calculate(operator);
-        operator = "subtract";
+        operator = 'subtract';
         break;
-      case "+":
+      case '+':
         calculate(operator);
-        operator = "add";
+        operator = 'add';
         break;
-      case "x":
+      case 'x':
         calculate(operator);
-        operator = "multiply";
+        operator = 'multiply';
         break;
-      case "÷":
+      case '÷':
         calculate(operator);
-        operator = "divide";
+        operator = 'divide';
         break;
-      case "=":
+      case '=':
         calculate();
         break;
     }
